d100e5c69a32c1da7c36b2ac991b1c544c2881a3
avkiseleva <avkiseleva@avkiseleva.msk.mirantis.net>
2015-02-09 16:44:46 +0300

    1. Script for getting access to nodes added. 2. Ability to get counter names from config or command line are added

--- README.md
+++ README.md
@@ -1,26 +1,41 @@
 # ceph_perf_counters
-Script to collect ceph counters from all osds and get some of them
+Script to collect ceph counters from all osds and get some of them.
 
-You need to have ssh access for all ceph nodes. You can do it like this: 
+You need to have ssh access for all ceph nodes. 
+You can do it manually like this: 
     1. Put your .pub file (with public key) on Fuel node (like this, for example scp .ssh/id_rsa.pub root@<fuel_node>:/root/myrsa.pub)
     2. Next send it from Fuel node by ssh-copy-id on all nodes from 'fuel --env-id <env_id> node list' (or only to controller and ceph nodes)
+Or you can use script ./get_access.sh <fuel_host> <environment_name>. This script uses algorithm, described above. You need to know root password of fuel host and to have local rsa key, named id_rsa. Note, that it is only for convenience, it can be not very safe or have any other problems. Read script for additional information.
 I'm sure, there are better ways :)
 
-Note, that now the most of options are setted directly in code, I will fix it later.
 
 perfcollect.py - contains functions, directly examining nodes. 
-	get_perf_dump_in_map - function, which returns perf dump for all osds in dict
+	get_perf_dump_in_map(host=cnt_host, user=default_user) - function, which returns perf dump for all osds in dict, it takes two optional parameters: controller host node and user name (root by default).
 
 get_perfs.py - program, which provide table output for optional counters. Counters are listed in code (temporary)
 
-Using:
+Usage:
 
-    python get_perfs.py [controller_host] [user_name]
+    python get_perfs.py [-h] [--ip IP] [--user USER] [--config CONFIG]
+                        [--collection COLLECTION [COLLECTION ...]]
 
-        controller_host - ip of controller node, isn't required if you set it in source or want to use default
-        user_name       - user name, via it you will access ssh, optional, "root" by default
+    Tool collects perf counters from ceph nodes and provides it in table format.
 
-        Output like this:
+    optional arguments:
+      -h, --help            show this help message and exit
+      --ip IP, -i IP        Controller host
+      --user USER, -u USER  User name for all hosts
+      --config CONFIG, -g CONFIG
+                            Use it, if you want upload needed counter names from
+                            file (json format, .counterslist as example)
+      --collection COLLECTION [COLLECTION ...], -c COLLECTION [COLLECTION ...]
+                            Counter collections in format collection_name counter1
+                            counter2 ...
+
+    Note, if you don't use one of -c or -g options, counter names from the code will be used.
+
+    You will get output like this:
+
             Collecting.....
             +-----------------------+-------------------+-------------------+
             |                       | osd0              | osd1              |
--- get_perfs.py
+++ get_perfs.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 
 import sys
+import argparse
 import json
 import texttable as TT
 import perfcollect as PC
@@ -10,20 +11,58 @@ perf_counters = {'WBThrottle': ['bytes_dirtied', 'ios_dirtied'],
                  'filestore': ['journal_latency', 'journal_queue_max_ops', 'commitcycle_interval']}
 
 
-def get_perfcounters_list():
-    clist = open(".counterslist").read()
+def get_perfcounters_list_from_config(config):
+# function to read config file
+    clist = open(config).read()
     return json.loads(clist)
+
+def get_perfcounters_list_from_sysargs(args):
+# function to get counters list from args
+    pc = dict()
+    for lst in args:
+        pc[lst[0]] = []
+        for i in range(1,len(lst)):
+            pc[lst[0]].append(lst[i])
+    return pc
 
 
 def main(): 
-    print (get_perfcounters_list())
-    if ( len(sys.argv) == 1 ):
-        perf_list = PC.get_perf_dump_in_map()
-    elif ( len(sys.argv) == 2 ):
-        perf_list = PC.get_perf_dump_in_map(sys.argv[1])
-    else:
-        perf_list = PC.get_perf_dump_in_map(sys.argv[1], sys.argv[2])
+    # parse command line
+    ag = argparse.ArgumentParser(description="Collect perf counters from ceph nodes",
+                                    epilog="Note, if you don't use both -c and -g options, counter names from the code will be used.")
+    ag.add_argument("--ip","-i",type=str,help="Controller host")
+    ag.add_argument("--user","-u",type=str,help="User name for all hosts")
+    ag.add_argument("--config","-g",type=str,help="Use it, if you want upload needed counter names from file (json format, .counterslist as example)")
+    ag.add_argument("--collection","-c",type=str,action="append",nargs='+',help="Counter collections in format collection_name counter1 counter2 ...")
+    args = ag.parse_args()
 
+    # check some errors in command line
+    if (args.collection != None):
+        for lst in args.collection:
+            if (len(lst) < 2):
+                print ("Collection argument must contain at least one counter")
+                return 1
+    if (args.config != None and args.collection != None):
+        print ("You cannot add counters from config and command line together")
+        return 1
+
+    # prepare info about needed counters
+    if (args.config != None):
+        perf_counters = get_perfcounters_list_from_config (args.config)
+    if (args.collection != None):
+        perf_counters = get_perfcounters_list_from_sysargs (args.collection)
+
+    # send parameters to external function
+    if (args.ip == None and args.user == None):
+        perf_list = PC.get_perf_dump_in_map()
+    elif (args.ip != None and args.user == None):
+        perf_list = PC.get_perf_dump_in_map(host=args.ip)
+    elif (args.ip == None and args.user != None):
+        perf_list = PC.get_perf_dump_in_map(user=args.user)
+    else:
+        perf_list = PC.get_perf_dump_in_map(host=args.ip,user=args.user)
+
+    # prepare data for table output
     tab = TT.Texttable()
     tab.set_deco(tab.HEADER | tab.VLINES | tab.BORDER | tab.HLINES)
 
@@ -50,7 +89,7 @@ def main():
             tab.add_row(row)
 
 
-    print tab.draw()
+    print (tab.draw())
 
 
 if __name__ == '__main__':
